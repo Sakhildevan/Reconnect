4fb0ccd7b1576e8bf90afd15e118d11e6d1b2495
Web-Abhinand <websollabsabhinand@gmail.com>
2022-06-22 15:39:55 +0530

    ninth commit

--- src/Alumini/alumini.js
+++ src/Alumini/alumini.js
@@ -20,12 +20,89 @@ function Alumini() {
       <div className='oooo'>
         <Grid container spacing={2}>
           <Grid item xs={6} md={3}>
-            <div class="uk-card uk-card-default">
+            <div className="uk-card uk-card-default">
               <div class="uk-card-media-top">
                 <img src={bg1} width="1800" height="1200" alt=""></img>
               </div>
               <div class="uk-card-body">
                 <h3 class="uk-card-title">Media Top</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={6} md={3}>
+            <div className="uk-card uk-card-default">
+              <div className="uk-card-media-top">
+                <img src={bg1} width="1800" height="1200" alt=""></img>
+              </div>
+              <div className="uk-card-body">
+                <h3 class="uk-card-title">Media Top</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={6} md={3}>
+            <div className="uk-card uk-card-default">
+              <div className="uk-card-media-top">
+                <img src={bg1} width="1800" height="1200" alt=""></img>
+              </div>
+              <div className="uk-card-body">
+                <h3 className="uk-card-title">Media Top</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={6} md={3}>
+            <div className="uk-card uk-card-default">
+              <div className="uk-card-media-top">
+                <img src={bg1} width="1800" height="1200" alt=""></img>
+              </div>
+              <div className="uk-card-body">
+                <h3 className="uk-card-title">Media Top</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={6} md={3}>
+            <div className="uk-card uk-card-default">
+              <div className="uk-card-media-top">
+                <img src={bg1} width="1800" height="1200" alt=""></img>
+              </div>
+              <div className="uk-card-body">
+                <h3 className="uk-card-title">Media Top</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={6} md={3}>
+            <div className="uk-card uk-card-default">
+              <div className="uk-card-media-top">
+                <img src={bg1} width="1800" height="1200" alt=""></img>
+              </div>
+              <div className="uk-card-body">
+                <h3 className="uk-card-title">Media Top</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={6} md={3}>
+            <div className="uk-card uk-card-default">
+              <div className="uk-card-media-top">
+                <img src={bg1} width="1800" height="1200" alt=""></img>
+              </div>
+              <div className="uk-card-body">
+                <h3 className="uk-card-title">Media Top</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={6} md={3}>
+            <div className="uk-card uk-card-default">
+              <div className="uk-card-media-top">
+                <img src={bg1} width="1800" height="1200" alt=""></img>
+              </div>
+              <div className="uk-card-body">
+                <h3 className="uk-card-title">Media Top</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
               </div>
             </div>
@@ -62,7 +139,18 @@ function Alumini() {
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
               </div>
             </div>
-          </Grid>                                                                        
+          </Grid>
+          <Grid item xs={6} md={3}>
+            <div class="uk-card uk-card-default">
+              <div class="uk-card-media-top">
+                <img src={bg1} width="1800" height="1200" alt=""></img>
+              </div>
+              <div class="uk-card-body">
+                <h3 class="uk-card-title">Media Top</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+              </div>
+            </div>
+          </Grid>                                                                              
         </Grid>
       </div>
     </>
